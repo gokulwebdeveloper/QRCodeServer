788ea1410ffad3fae79afe7cba7a6373dd08110d
Gokul K <gokulwebdeveloper@gmail.com>
2020-03-30 15:02:22 +0530

    new changes in the code

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var express = require('express'),
 const cors = require('cors');
 var fs = require('fs');
 var url = require('url');
-var chmodr = require('chmodr');
-
 app.use(cors())    
 var bodyParser = require('body-parser');
 app.use('/qrcode', express.static(process.cwd() + '/qrcode'));
@@ -16,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
    extended: true
 }));
-
+const storage = require('@google-cloud/storage');
 app.use(bodyParser.json());
 function makeid(length) {
    var result           = '';
@@ -38,17 +36,10 @@ app.post('/',function(req,res){
    let base64String = req.body[0].data; // Not a real image
     let base64Image = base64String.split(';base64,').pop();
     let filename =  makeid(7);
-    chmodr('qrcode/', 0o777, (err) => {
-      if (err) {
-        console.log('Failed to execute chmod', err);
-      } else {
-        console.log('Success');
-      }
-    });
-     fs.writeFile('qrcode/'+filename+'.png', base64Image, {encoding: 'base64'}, function(err) {
+    fs.writeFile(filename+'.png', base64Image, {encoding: 'base64'}, function(err) {
 	    console.log('File created');
 	});
-    res.json({'qrCodeURL':requrl+'qrcode/'+filename+'.png'});
+    res.json({'qrCodeURL':requrl+filename+'.png'});
 });
 
 app.listen(port);
